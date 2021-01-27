257759369a63e4888665a210fd105f4eed8256ed
smerritt3nr2 <smerritt3nr2@yahoo.com>
2021-01-27 11:23:43 +0000

    leaflet + recaptcha

---
+++ js/recaptcha.js
@@ -0,0 +1,16 @@
+/*================================================
+ * Main JavaScript Engine
+ =================================================*/
+function get_action(form) {
+    var v = grecaptcha.getResponse();
+    if(v.length == 0){
+        document.getElementById('captcha').innerHTML="You can't leave Captcha Code empty";
+        return false;
+    }
+    else {
+        document.getElementById('captcha').innerHTML="Captcha completed";
+        return true; 
+    }
+}
+ /*================================================*/
+
